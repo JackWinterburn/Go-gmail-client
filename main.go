74b2154467ef78c006679fc8160ac68525900002
JackWinterburn <jack.winterburn4@hotmail.co.uk>
2020-10-26 17:59:54 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var templates = template.Must(template.ParseFiles("templates/home.html", "templates/login.html"))
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "home.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "login.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	//home page
+	http.HandleFunc("/", homeHandler)
+
+	//login page
+	http.HandleFunc("/login", loginHandler)
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
+	fmt.Println("Server running on port 3000")
+}
